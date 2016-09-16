a677e3d612bcb61a1a4dcc3da0febfebd2f16807
henmerlin <henmerlin@gmail.com>
2016-09-16 19:38:15 -0300

    Comit!

---
+++ PesquisaEleitoral_MVC/PesquisaEleitoral_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PesquisaEleitoral_MVC.Startup))]
+namespace PesquisaEleitoral_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
